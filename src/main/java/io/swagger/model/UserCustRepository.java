da522a1890bb72a2769e2f9056ea3e4c7c66e382
enigmasck <enigmasck@172.20.10.4>
2019-04-12 08:55:13 +0200

    changed named of the repo

---
+++ src/main/java/io/swagger/model/UserCustRepository.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.swagger.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserCustRepository extends CrudRepository<User, Integer> {
+
+}
