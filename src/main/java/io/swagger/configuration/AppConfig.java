d5796deb39ce32646e6d75069d1665a133436182
enigmasck <timothyasanders@gmail.com>
2019-04-13 14:40:07 +0200

    LORD MARY JEBSUS I GOT IT WORKING - ITS PULLING DATA FROM MYSQL!!!! HOLY SHITTTTTTTTTTTTTTTTTTTTTTTTTTTT

--- src/main/java/io/swagger/configuration/AppConfig.java
+++ src/main/java/io/swagger/configuration/AppConfig.java
@@ -9,6 +9,7 @@ import javax.activation.DataSource;
 import javax.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
