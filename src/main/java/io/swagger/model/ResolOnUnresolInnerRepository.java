d9f4c8cb727e4cdf9c049aa608727817648eae64
sony <sony@DESKTOP-5ASO223>
2019-04-15 15:30:33 +0200

    ResolOnUnresolInnerRepository

---
+++ src/main/java/io/swagger/model/ResolOnUnresolInnerRepository.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.swagger.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author sony
+ */
+public interface ResolOnUnresolInnerRepository extends CrudRepository<ResolOnUnresolInner, Integer>{
+    
+}
